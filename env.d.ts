6a1a0fa913710b709faea8e6a52d81bb25c277ab
Brian Takita <brian.takita@gmail.com>
2020-07-08 01:13:49 -0700

    + index.ts

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,2 @@
 export declare const AUTH0_URL: string;
+export declare function validate__auth0__env(): void;
--- env.ts
+++ env.ts
@@ -2,7 +2,9 @@ import { throw__missing__env } from '@ctx-core/env/env'
 import { _AUTH0_URL } from './package'
 import { log } from '@ctx-core/logger'
 const logPrefix = '@ctx-core/auth0/env'
-log(logPrefix)
 export const AUTH0_URL = _AUTH0_URL()
-if (!process.env.AUTH0_CLIENT_ID) throw__missing__env('AUTH0_CLIENT_ID')
-if (!process.env.AUTH0_DOMAIN) throw__missing__env('AUTH0_DOMAIN')
+export function validate__auth0__env() {
+	log(logPrefix)
+	if (!process.env.AUTH0_CLIENT_ID) throw__missing__env('AUTH0_CLIENT_ID')
+	if (!process.env.AUTH0_DOMAIN) throw__missing__env('AUTH0_DOMAIN')
+}
