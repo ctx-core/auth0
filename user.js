d7ee9bc96bdc54e9674d6db28bda9e46d38501f7
Brian Takita <brian.takita@gmail.com>
2020-08-05 17:38:09 -0700

    ts outputs esnext

--- user.js
+++ user.js
@@ -1,9 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._user_id = void 0;
-function _user_id(decoded__token__jwt) {
+export function _user_id(decoded__token__jwt) {
     return (decoded__token__jwt
         && (decoded__token__jwt.user_id
             || decoded__token__jwt.sub));
 }
-exports._user_id = _user_id;
