42cf79e733d586c8812945a5742021f8578892bc
Brian Takita <brian.takita@gmail.com>
2021-12-14 03:14:34 -0600

    update dependencies

--- src/password_realm_body__b.ts
+++ src/password_realm_body__b.ts
@@ -2,9 +2,7 @@ import { be_, assign, Ctx } from '@ctx-core/object'
 import type { auth0_client_id_optional_body_I } from './auth0_client_id_body_I.js'
 import type { auth0_client_id_body_I } from './auth0_client_id_body_I.js'
 import type { auth0_grant_type_body_I } from './auth0_grant_type_body_I.js'
-export function password_realm_body__b</*@formatter:off*/
-	Out extends password_realm_body_T = password_realm_body_T
-/*@formatter:on*/>(
+export function password_realm_body__b<Out extends password_realm_body_T = password_realm_body_T>(
 	ctx:Ctx, key:string, auth0_body_fn:(body:auth0_client_id_optional_body_I)=>Out
 ):password_realm_body__T<Out> {
 	return be_<password_realm_body__T<Out>>(key, ()=>{
