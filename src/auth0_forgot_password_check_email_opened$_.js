c314e59f3a67f99c630599e5daa638b6dc6532a2
Brian Takita <brian.takita@gmail.com>
2022-01-31 18:29:14 -0600

    inlined const key

--- src/auth0_forgot_password_check_email_opened$_.js
+++ src/auth0_forgot_password_check_email_opened$_.js
@@ -1,9 +1,8 @@
 import { computed$ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
 import { auth0_opened$_ } from './auth0_opened$_.js'
-const key = 'auth0_forgot_password_check_email_opened$'
 /** @type {import('auth0_forgot_password_check_email_opened$_.d.ts').auth0_forgot_password_check_email_opened$_} */
-export const auth0_forgot_password_check_email_opened$_ = be_(key, (
+export const auth0_forgot_password_check_email_opened$_ = be_('auth0_forgot_password_check_email_opened$', (
 	ctx
 )=>
 	computed$(auth0_opened$_(ctx),
--- src/auth0_login_opened$_.js
+++ src/auth0_login_opened$_.js
@@ -1,9 +1,8 @@
 import { computed$ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
 import { auth0_opened$_ } from './auth0_opened$_.js'
-const key = 'auth0_login_opened$'
 /** @type {import('./auth0_login_opened$_.d.ts').auth0_login_opened$_} */
-export const auth0_login_opened$_ = be_(key, ctx=>
+export const auth0_login_opened$_ = be_('auth0_login_opened$', ctx=>
 	(computed$(auth0_opened$_(ctx),
 		auth0_login_opened=>
 			!auth0_login_opened
