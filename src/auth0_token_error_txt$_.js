f6252feb171167a26a403a03b986540bd1ace059
Brian Takita <brian.takita@gmail.com>
2022-02-06 17:28:33 -0600

    auth0_token_error_txt$_: formatting

--- src/auth0_token_error_txt$_.js
+++ src/auth0_token_error_txt$_.js
@@ -3,15 +3,13 @@ import { be_ } from '@ctx-core/object'
 import { auth0_token_error$_ } from './auth0_token_error$_.js'
 /** @type {import('./auth0_token_error_txt$_.d.ts').auth0_token_error_txt$_} */
 export const auth0_token_error_txt$_ = be_('auth0_token_error_txt$', ctx=>
-	computed$(auth0_token_error$_(ctx), auth0_token_error=>
-		auth0_token_error
-		? auth0_token_error.error_message
-			|| auth0_token_error.message
-			? auth0_token_error.error_message
-				|| auth0_token_error.message
-			: auth0_token_error.error_description
-				? auth0_token_error.error_description
-				: auth0_token_error.error
-					? auth0_token_error.error
+	computed$(auth0_token_error$_(ctx), $=>
+		$
+		? $.error_message || $.message
+			? $.error_message || $.message
+			: $.error_description
+				? $.error_description
+				: $.error
+					? $.error
 					: ''
 		: ''))
