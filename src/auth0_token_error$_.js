70cbca7fd6a5c90cf3fd8838fd0213bde84b9b14
Brian Takita <brian.takita@gmail.com>
2022-02-07 18:08:40 -0600

    auth0_token_error$_: fix: when auth0_opened changes: set to null

--- src/auth0_token_error$_.js
+++ src/auth0_token_error$_.js
@@ -1,5 +1,15 @@
 import { atom$ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
+import { auth0_opened$_ } from './auth0_opened$_.js'
 /** @type {import('auth0_token_error$_.d.ts').auth0_token_error$_} */
-export const auth0_token_error$_ = be_('auth0_token_error$', ()=>
-	atom$(undefined))
+export const auth0_token_error$_ = be_('auth0_token_error$', ctx=>{
+	const auth0_token_error$ = atom$(null)
+	let auth0_opened
+	auth0_opened$_(ctx).subscribe($=>{
+		if (auth0_opened !== $) {
+			auth0_opened = $
+			auth0_token_error$.$ = null
+		}
+	})
+	return auth0_token_error$
+})
