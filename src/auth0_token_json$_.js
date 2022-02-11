838787a0381436df2b5a6aaf34819422d7c46620
Brian Takita <brian.takita@gmail.com>
2022-02-11 03:22:22 -0600

    auth0_token_json$_: listen: set localStorage data

--- src/auth0_token_json$_.js
+++ src/auth0_token_json$_.js
@@ -1,6 +1,13 @@
 import { has_dom } from '@ctx-core/dom'
 import { neql_atom$ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
+const localStorage_key = 'auth0_token_json'
 /** @type {import('./auth0_token_json$_.d.ts').auth0_token_json$_} */
-export const auth0_token_json$_ = be_('auth0_token_json$', ()=>
-	neql_atom$(has_dom && localStorage.getItem('auth0_token_json') || null))
+export const auth0_token_json$_ = be_('auth0_token_json$', ()=>{
+	const auth0_token_json$ = neql_atom$(has_dom && localStorage.getItem(localStorage_key) || null)
+	if (has_dom) {
+		auth0_token_json$.listen($=>
+			localStorage.setItem(localStorage_key, $))
+	}
+	return auth0_token_json$
+})
