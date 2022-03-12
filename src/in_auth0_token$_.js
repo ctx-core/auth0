834210f0148c8cd4b3de739d28999fba07cd1f3a
Brian Takita <brian.takita@gmail.com>
2022-03-12 15:31:07 -0600

    in_auth0_token$_: memoizing auth0_token_json instead of running JSON.parse($)

--- src/in_auth0_token$_.js
+++ src/in_auth0_token$_.js
@@ -3,14 +3,13 @@ import { be_ } from '@ctx-core/object'
 import { auth0_token_json$_ } from './auth0_token_json$_.js'
 /** @type {import('./in_auth0_token$_.d.ts').in_auth0_token$_} */
 export const in_auth0_token$_ = be_('in_auth0_token$', ctx=>{
-	let in_auth0_token
+	let auth0_token_json, in_auth0_token
 	const in_auth0_token$ = computed$(auth0_token_json$_(ctx), $=>{
 		if ($ && typeof $ === 'string') {
 			try {
-				const current_in_auth0_token_json = JSON.stringify(in_auth0_token)
-				if (current_in_auth0_token_json !== $) {
-					in_auth0_token = JSON.parse($) || null
-				}
+				if (auth0_token_json === $) return in_auth0_token
+				auth0_token_json = $
+				in_auth0_token = JSON.parse(auth0_token_json)
 				return in_auth0_token
 			} catch (err) {
 				queueMicrotask(()=>auth0_token_json$_(ctx).$ = null)
