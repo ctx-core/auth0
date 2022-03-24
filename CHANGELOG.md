bdb7328a36e841872424ec8ddfc6a3a8f752b0f5
Brian Takita <brian.takita@gmail.com>
2022-03-24 00:22:24 -0500

    ## 34.2.1
    ### Patch Changes
    
    - auth0*logout_msg\$*: fix: subscribe: queueMicrotask to auth0_logout_msg$.$ = null
    
    fix: auth0*auth0_login_opened\$\\_\$*:\$ === 'login'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/auth0
 
+## 34.2.1
+
+### Patch Changes
+
+- auth0*logout_msg\$*: fix: subscribe: queueMicrotask to auth0_logout_msg$.$ = null
+
 ## 34.2.0
 
 ### Minor Changes
@@ -295,7 +301,7 @@
 - fix: auth0*token\$*,in*auth0_token\$*,validate_auth0_change_password,validate_auth0_password_confirmation: remove false state: null instead
 - fix: auth0*change_password_opened\$*: \$ === 'change_password'
 
-  fix: auth0*auth0_login_opened$\_\$*:\$ === 'login'
+  fix: auth0*auth0_login_opened\$\\_\$*:\$ === 'login'
 
 ## 32.0.2
 
--- src/auth0_logout_msg$_.js
+++ src/auth0_logout_msg$_.js
@@ -2,13 +2,19 @@ import { atom$ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
 import { logout_auth0_token } from './logout_auth0_token.js'
 /** @type {import('./auth0_logout_msg$_.d.ts').auth0_logout_msg$_} */
-export const auth0_logout_msg$_ = be_('auth0_logout_msg$', ()=>
-	atom$(undefined))
+export const auth0_logout_msg$_ = be_('auth0_logout_msg$', ()=>{
+	const auth0_logout_msg$ = atom$(undefined)
+	auth0_logout_msg$.subscribe($=>{
+		if ($) {
+			queueMicrotask(()=>auth0_logout_msg$.$ = null)
+		}
+	})
+	return auth0_logout_msg$
+})
 /**
  * @param {import('@ctx-core/object').Ctx}ctx
  */
 export function auth0_logout(ctx) {
 	logout_auth0_token(ctx)
 	auth0_logout_msg$_(ctx).$ = { time: Date.now() }
-	queueMicrotask(()=>auth0_logout_msg$_(ctx).$ = null)
 }
