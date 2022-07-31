d3cf7c594b3f5a24a1a4b9978a71918cf83cd75c
Brian Takita <brian.takita@gmail.com>
2022-07-31 03:11:35 -0500

    ## 34.4.15
    ### Patch Changes
    
    - AUTH0*URL*: fix: import auth0-js/package.json
    - AUTH0*URL*: synchronous function
    
    fix: auth0*auth0_login_opened\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\$\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\$*:\$ === 'login'

--- src/AUTH0_URL_/index.js
+++ src/AUTH0_URL_/index.js
@@ -1,4 +1,4 @@
-import pkg from 'auth0-js/package.json' assert { type: 'json' }
+import pkg from 'auth0-js/package.json'
 /** @type {import('./AUTH0_URL_.d.ts').AUTH0_URL_} */
-export const AUTH0_URL_ = async ()=>`https://cdn.auth0.com/js/auth0/${pkg.version}/auth0.min.js`
+export const AUTH0_URL_ = ()=>`https://cdn.auth0.com/js/auth0/${pkg.version}/auth0.min.js`
 export { AUTH0_URL_ as _AUTH0_URL }
