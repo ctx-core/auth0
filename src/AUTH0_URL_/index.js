5ad9009bee97a3c39fcff060d2f7b18dc443ea0a
Brian Takita <brian.takita@gmail.com>
2022-07-31 03:38:27 -0500

    ## 34.4.16
    ### Patch Changes
    
    - AUTH0*URL*: async,import in function
    
    fix: auth0*auth0_login_opened\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\$\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\$*:\$ === 'login'

--- src/AUTH0_URL_/index.js
+++ src/AUTH0_URL_/index.js
@@ -1,4 +1,5 @@
-import pkg from 'auth0-js/package.json'
 /** @type {import('./AUTH0_URL_.d.ts').AUTH0_URL_} */
-export const AUTH0_URL_ = ()=>`https://cdn.auth0.com/js/auth0/${pkg.version}/auth0.min.js`
+export const AUTH0_URL_ = ()=>
+	import('auth0-js/package.json', { assert: { type: 'json' } })
+		.then($=>`https://cdn.auth0.com/js/auth0/${$.version}/auth0.min.js`)
 export { AUTH0_URL_ as _AUTH0_URL }
--- src/AUTH0_URL_/index.test.ts
+++ src/AUTH0_URL_/index.test.ts
@@ -1,7 +1,7 @@
 import { test } from 'uvu'
 import { equal } from 'uvu/assert'
 import { AUTH0_URL_ } from '../index.js'
-test('invert_idx_a_', ()=>{
-	equal('https://cdn.auth0.com/js/auth0/9.19.0/auth0.min.js', AUTH0_URL_())
+test('invert_idx_a_', async ()=>{
+	equal('https://cdn.auth0.com/js/auth0/9.19.0/auth0.min.js', await AUTH0_URL_())
 })
 test.run()
