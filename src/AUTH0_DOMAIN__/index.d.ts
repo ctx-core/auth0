523112a17d0dfcb36c1f2432aef7673cf57f7f5f
Brian Takita <brian.takita@gmail.com>
2023-02-17 01:13:54 -0500

    ## 36.2.3
    ### Patch Changes
    
    - AUTH0_DOMAIN\_\_set: fix type name

--- src/AUTH0_DOMAIN__/index.d.ts
+++ src/AUTH0_DOMAIN__/index.d.ts
@@ -2,4 +2,4 @@ import type { WritableAtom_ } from '@ctx-core/nanostores'
 import type { Be, Ctx } from '@ctx-core/object'
 export declare const AUTH0_DOMAIN__:Be<WritableAtom_<string>>
 export { AUTH0_DOMAIN__ as AUTH0_DOMAIN$_ }
-export function AUTH0r_DOMAIN__set(ctx:Ctx, AUTH0_DOMAIN:string):void
+export function AUTH0_DOMAIN__set(ctx:Ctx, AUTH0_DOMAIN:string):void
